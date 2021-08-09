16eed942c21b20a28837646a57160284aa12a09f
cdmuir <chrisdmuir@gmail.com>
2021-08-09 00:50:59 -0700

    initial commit

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' @title Stomatal anatomy
+#' @description The data set contains stomatal anatomical characters (density, length, width, pore length, and pore width) for the ab- and adaxial leaf surface in a number of species synthesized from the primary literature.
+#' @format A data frame with 5786 rows and 7 variables:
+#' \describe{
+#'   \item{\code{source_id}}{character Taxonomic name give by the source paper.}
+#'   \item{\code{trait}}{character Description of trait.}
+#'   \item{\code{mu}}{double Trait mean.}
+#'   \item{\code{n}}{double Sample size (see \code{stomata_anatomy_metadata} for more information.)}
+#'   \item{\code{sd}}{double Trait standard deviation.} 
+#'   \item{\code{source}}{character Source paper identifier.} 
+#'   \item{\code{se}}{double Trait standard error.} 
+#'}
+"stomata_anatomy"
+
+#' @title Stomatal anatomy metadata
+#' @description This contains metadata for stomata_anatomy
+#' @format A data frame with 37 rows and 7 variables:
+#' \describe{
+#'   \item{\code{source}}{character Source paper identifier.} 
+#'   \item{\code{taxa}}{character Description of the taxa covered in source.}
+#'   \item{\code{location}}{character Location where trait data are located in source. In some cases, raw data were provided directly by the authors.}
+#'   \item{\code{density_unit}}{character Unit used for stomatal density.}
+#'   \item{\code{length_unit}}{character Unit used for guard cell or pore length or width.}
+#'   \item{\code{sample_size}}{double Approximate sample size for each taxon in the study.} 
+#'   \item{\code{notes}}{character Notes to understand units or sample size data.} 
+#'}
+"stomata_anatomy_metadata"
---
+++ README.md
@@ -0,0 +1,41 @@
+
+# ropenstomata
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+**ropenstomata** is an open database of stomatal anatomy synthesized from the primary literature.
+
+## Installation
+
+To instead install the latest development version of the package from GitHub use
+
+``` r
+# install.packages("remotes")
+remotes::install_github("cdmuir/ropenstomata")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(ropenstomata)
+
+# data set
+stomata_anatomy
+
+# metadata
+stomata_anatomy_metadata
+```
+
+## Contribute
+
+If you have data you would like to contribute to **ropenstomata**, please contact [Chris Muir](mailto:cdmuir@hawaii.edu).
+
+## Meta
+
+-   Please [report any issues or bugs](https://github.com/cdmuir/ropenstomata/issues).
+-   License: MIT
+-   Get citation information for **ropenstomata** in R doing `citation(package = 'ropenstomata')`
+-   Please note that the ropenstomata project is released with a [Contributor Code of Conduct](https://contributor-covenant.org/version/2/0/CODE_OF_CONDUCT.html). By contributing to this project, you agree to abide by its terms.
